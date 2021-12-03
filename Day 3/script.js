4936d8a97d3f25b20d145f6ba13395be0f11b2b7
Gabrielle Davidson <gdvdsn@gmail.com>
2021-12-03 10:37:04 -1000

    Completed Day 3/Solution 1, made helper functions more descriptive, and cleaned up console.logs

--- Day 3/script.js
+++ Day 3/script.js
@@ -4,7 +4,33 @@ const helpers = require("../helpers");
 //SOLUTION 1
 const calculatePowerConsumption = () => {
   fs.readFile("./report.txt", (err, data) => {
-    const report = helpers.convertDataToNumericArray(data);
+    const report = helpers.convertDataToArrayOfStrings(data);
+    let zeroCount = 0;
+    let oneCount = 0;
+    let gammaRate = "";
+    let epsilonRate = "";
+
+    for (var i = 0; i < report[0].length; i++) {
+      for (var j = 0; j < report.length; j++) {
+        if (report[j][i] === "1") {
+          oneCount++;
+        } else {
+          zeroCount++;
+        }
+      }
+
+      if (zeroCount > oneCount) {
+        gammaRate += "0";
+        epsilonRate += "1";
+      } else {
+        gammaRate += "1";
+        epsilonRate += "0";
+      }
+      zeroCount = 0;
+      oneCount = 0;
+    }
+
+    return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
   });
 };
 
